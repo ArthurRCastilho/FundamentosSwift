bc6701b8a07a270878ac600a15dcb67dd004a361
ArthurLVidal <arthurodri2017@gmail.com>
2024-12-16 08:50:26 -0300

    First Commit

---
+++ Fundamentos Swift.playground/Pages/DesafioVariaveis.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+var inicialDoNome: Character = "A"
+var nome: String = "Arthur"
+var apelido: String = "V1dal1"
+var idade: Int = 21
+var altura: Double = 1.65
+var gostaDeSwift: Bool = true
+
+print(inicialDoNome)
+print(nome)
+print(apelido)
+print(idade)
+print(altura)
+print(gostaDeSwift)
---
+++ Fundamentos Swift.playground/Pages/Variaveis.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+var nome: String = "Arthur"
+var idade: Int = 21
+
+print("o meu nome é: \(nome)")
+print("E minha idade é: \(idade)")
+
+var coeficienteDeRendimento: Double = 9.5
+var passouNaPrimeiraAula: Bool
+
+passouNaPrimeiraAula = false
+
+var caractere: Character = "D"
+print(caractere)
+
+typealias Aluno = String
+
+var aluno1: Aluno = "Arthur"
+var aluno2: Aluno = "João"
+var aluno3: Aluno = "Pedro"
+
+print(aluno1)
+print(aluno2)
+print(aluno2)
