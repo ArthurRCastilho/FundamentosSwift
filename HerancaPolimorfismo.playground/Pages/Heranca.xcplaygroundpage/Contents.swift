64f8bcaba64499172358901adbbf02e69743b67c
ArthurLVidal <arthurodri2017@gmail.com>
2024-12-19 17:28:56 -0300

    Adicionando Herança

--- HerancaPolimorfismo.playground/Pages/Heranca.xcplaygroundpage/Contents.swift
+++ HerancaPolimorfismo.playground/Pages/Heranca.xcplaygroundpage/Contents.swift
@@ -1,7 +1,45 @@
-//: [Previous](@previous)
+class Conta {
+    var saldo = 0.0
+    var nome: String
+    
+    func sacar(_ valor: Double) {
+        saldo -= valor
+    }
+    
+    func depositar(_ valor: Double) {
+        saldo += valor
+    }
+    
+    init(saldo: Double = 0.0, nome: String) {
+        self.saldo = saldo
+        self.nome = nome
+    }
+}
 
-import Foundation
+class ContaPoupanca: Conta { // Classe que herda tudo da classe conta
+    // A conta poupança tem os mesmos atributos e funções da classe Conta
+    var possuiCartaoDebito = false
+    func solicitarDebito() {
+        possuiCartaoDebito = true
+        print("Agora tem cartão débito")
+    }
+}
 
-var greeting = "Hello, playground"
+class ContaCorrente: Conta {
+    // O mesmo acontece com conta corrente
+    func solicitarEmprestimo(_ valor: Double) {
+        saldo += valor
+        print("O cliente está solicitando emprestimo do valor de \(valor)")
+        print("Agora seu saldo é de: \(saldo)")
+    }
+}
 
-//: [Next](@next)
+var contaPoupanca = ContaPoupanca(nome: "Arthur")
+contaPoupanca.depositar(50) // Aqui já é possível perceber que
+print(contaPoupanca.saldo)
+contaPoupanca.solicitarDebito() // Este método não está disponível na contaCorrente
+
+var contaCorrente = ContaCorrente(nome: "Arthur")
+contaCorrente.depositar(5000)
+print(contaCorrente.saldo)
+contaCorrente.solicitarEmprestimo(2000)
